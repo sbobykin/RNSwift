4123a4e1f932ccceb7984e943843318524232b38
Stanislav Bobykin <sbobykin@gmail.com>
2017-12-21 08:51:41 +0500

    adding of SwiftView.swift with bridging header

---
+++ ios/RNSwift/SwiftView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftView.swift
+//  RNSwift
+//
+//  Created by Apel Yl on 21/12/2017.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+import UIKit
+
+class SwiftView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
