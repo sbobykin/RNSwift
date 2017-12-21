34196772b5d9d384160d1afca29631639cf55f65
Stanislav Bobykin <sbobykin@gmail.com>
2017-12-21 08:23:11 +0500

    adding of SwiftView manager

---
+++ ios/RNSwift/SwiftViewManager.h
@@ -0,0 +1,13 @@
+//
+//  SwiftViewManager.h
+//  RNSwift
+//
+//  Created by Apel Yl on 21/12/2017.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+#import "RCTViewManager.h"
+
+@interface SwiftViewManager : RCTViewManager
+
+@end
---
+++ ios/RNSwift/SwiftViewManager.m
@@ -0,0 +1,13 @@
+//
+//  SwiftViewManager.m
+//  RNSwift
+//
+//  Created by Apel Yl on 21/12/2017.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+#import "SwiftViewManager.h"
+
+@implementation SwiftViewManager
+
+@end
