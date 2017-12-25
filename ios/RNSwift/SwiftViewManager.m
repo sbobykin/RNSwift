1f74178feaa45c61159b43ef49b078b6fbaa5cdf
Stanislav Bobykin <sbobykin@gmail.com>
2017-12-25 15:20:15 +0500

    calling of onSwiftButtonPressed from SwiftViewManager

--- ios/RNSwift/SwiftViewManager.m
+++ ios/RNSwift/SwiftViewManager.m
@@ -11,16 +11,23 @@ RCT_EXPORT_MODULE()
 RCT_EXPORT_VIEW_PROPERTY(text, NSString)
 RCT_EXPORT_VIEW_PROPERTY(onSwiftButtonPressed, RCTBubblingEventBlock)
 
+SwiftView* swiftView;
+
 - (UIView *) view
 {
-  SwiftView* swiftView = [SwiftView new];
+  swiftView = [SwiftView new];
   swiftView.delegate = self;
   return swiftView;
   //return [[SwiftView alloc] init];
 }
 
 - (void)swiftButtonTapped {
-  NSLog(@"SwiftViewManager: swiftButtonTapped");
+  
+  if(!swiftView.onSwiftButtonPressed) {
+    return;
+  }
+  
+  swiftView.onSwiftButtonPressed(@{});
 }
 
 @end
