49a7f1be48281019e29522e7ad1b5801cc6d8cde
Stanislav Bobykin <sbobykin@gmail.com>
2017-12-25 16:38:18 +0500

    prop-types

--- SwiftView.js
+++ SwiftView.js
@@ -1,3 +1,20 @@
+import React from 'react';
 import { requireNativeComponent } from 'react-native';
+import PropTypes from 'prop-types';
 
-export default requireNativeComponent('SwiftView', null);
+
+class SwiftView extends React.Component {
+  render() {
+    return <NativeSwiftView {...this.props} />
+  }
+}
+
+SwiftView.propTypes = {
+  text: PropTypes.string,
+  onSwiftButtonPressed: PropTypes.func
+};
+
+let NativeSwiftView = requireNativeComponent('SwiftView', SwiftView);
+
+export default SwiftView;
+
